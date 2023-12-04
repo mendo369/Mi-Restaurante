36265908001f6b3294ec3e3e88ccff530cbce8c2
Luis Mendoza <luis23luis33@gmail.com>
2023-12-04 12:39:55 -0500

    hash password with sha256

--- Backend/Auth/auth.py
+++ Backend/Auth/auth.py
@@ -1,11 +1,20 @@
-def register(email, contraseña):
+import hashlib
+
+def register(email, password):
 
   if not email_validate(email):
     print("error")
     return False
+  
+  if not password_validate_and_hash(password):
+    print("Contraseña no valida")
+    return False
+  
+  password = password_validate_and_hash(password)
+
 
   db = open("registro_inicio.txt", "a")
-  db.write(f"{email}, {contraseña}\n")
+  db.write(f"{email}, {password}\n")
   db.close()
 
   print("user save")
@@ -31,5 +40,34 @@ def email_validate(email):
 
     return False
 
+def password_validate_and_hash(password):
+  if len(password) < 10:
+    return None
+
+  # Comprobamos que la contraseña tenga al menos 1 letra minúscula.
+
+  if not any(c.islower() for c in password):
+    return None
+
+  # Comprobamos que la contraseña tenga al menos 1 letra mayúscula.
+
+  if not any(c.isupper() for c in password):
+    return None
+
+  # Comprobamos que la contraseña tenga al menos 1 número.
+
+  if not any(c.isdigit() for c in password):
+    return None
+
+  # Comprobamos que la contraseña tenga al menos 1 carácter especial.
+
+  caracteres_especiales = "@*$!?\&/"
+  if not any(c in caracteres_especiales for c in password):
+    return None
+
+  # Encriptamos la contraseña con SHA-256.
+
+  return hashlib.sha256(password.encode()).hexdigest()
+
 if __name__ == "__main__":
-  register("luis@gmail.com", "password")
+  register("luis@gmail.com", "Ld$123mend")
