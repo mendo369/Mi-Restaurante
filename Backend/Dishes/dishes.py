16f26f1345475f4fa693b8a9ccb6634429438481
Luis Mendoza <luis23luis33@gmail.com>
2023-12-05 14:42:35 -0500

    Delete dish

--- Backend/Dishes/dishes.py
+++ Backend/Dishes/dishes.py
@@ -21,7 +21,32 @@ def create_dish(name, description, price, available):
    db.close()
    return True
 
+def delete_dish(dish_name):
+    dishes = get_dishes()
+    print(dishes)
+    index = None
+
+    for i, diccionario in enumerate(dishes):
+        if diccionario["name"] == dish_name:
+            index = i
+            break
+
+    if index is None:
+        print(f"El diccionario con nombre '{dish_name}' no se encontró en la lista")
+        return False
+    
+    del dishes[index]
+
+    db = open("dishes.txt", "w")
+    for dish in dishes:
+            db.write(json.dumps(dish) + "\n")
+    db.close()
+
+    return dishes
+
 if __name__=="__main__":
-#    create_dish("Flan", "un flan de queso",8000, True)
+#    create_dish("Flan3", "un flan de queso",8000, True)
    dishes =get_dishes()
-   print(dishes)
+#    print(dishes)
+   delete = delete_dish("Flan3")
+#    print(delete)
